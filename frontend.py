10c739a556973d5e81408e956a941aee64af46e7
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-04-28 12:40:07 +0800

    connecting the button and adding functions to the DB

--- frontend.py
+++ frontend.py
@@ -1,7 +1,61 @@
 from tkinter import *
 import backend
 
+#this is allows us retrieve the selected row and matching index
+def get_selected_row(event):
+    global selected_tuple
+    #first we need to get the list from the listbox
+    index=list1.curselection()[0]
+  #to get the tupule of all the row
+  #selected variable is a local variable its inside a function
+    selected_tuple=list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4])
+
+
+
+
+def view_command():
+    #creating the empty list box to give all the data in DB
+    list1.delete(0,END)
+    #to inseert the tuples from the list[( )]
+    for row in backend.view():
+        list1.insert(END,row)
+
+
+def search_command():
+    #first we empty the list
+    list1.delete(0,END)
+    # iterate with a loop to fina a single output string
+    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+        list1.insert(END,row)
+
+
+
+def add_command():
+    backend.insert(title_text.get(), author_text.get(),year_text.get(),isbn_text.get())
+    #first we make sure the list is empty
+    list1.delete(0,END)
+    list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+
+
+def update_command():
+    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+
+
 window=Tk()
+
+window.wm_title("BookStore")
 
 
 #ctreating the grid and columns
@@ -53,27 +107,31 @@ list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
 
+#tkinter bind allows us to highled selected row
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
+
 #BUTTONS
-b1=Button(window,text="View all", width=12)
+b1=Button(window,text="View all", width=12,command=view_command)
 b1.grid(row=2, column=3)
 
 
-b2=Button(window,text="Search entry", width=12)
+b2=Button(window,text="Search entry", width=12,command=search_command)
 b2.grid(row=3, column=3)
 
 
-b3=Button(window,text="Add entry", width=12)
+b3=Button(window,text="Add entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
 
-b4=Button(window,text="Update selected", width=12)
+b4=Button(window,text="Update selected", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
-b5=Button(window,text="Delete selected", width=12)
+b5=Button(window,text="Delete selected", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
 
-b6=Button(window,text="Close", width=12)
+b6=Button(window,text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 
